107624f2a8a388e496797602f160add3380fd538
Ryan Graves <metaryan@byu.edu>
2020-10-21 20:09:58 -0600

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
         Calculator calc = new Calculator();
-        int arg1 = Integer.parseInt(args[1]);
-        int arg2 = Integer.parseInt(args[2]);
-        if (args[0] == "add") {
+        System.out.println("Enter a command:");
+        Scanner sc = new Scanner (System.in);
+        String input = sc.next();
+        String[] arguments = input.split(" ");
+        int arg1 = Integer.parseInt(arguments[1]);
+        int arg2 = Integer.parseInt(arguments[2]);
+        if (arguments[0] == "add") {
             System.out.println(calc.add(arg1, arg2));
-        } else if (args[0] == "subtract") {
+        } else if (arguments[0] == "subtract") {
             System.out.println(calc.subtract(arg1, arg2));
-        } else if (args[0] == "multiply") {
+        } else if (arguments[0] == "multiply") {
             System.out.println(calc.multiply(arg1, arg2));
-        } else if (args[0] == "divide") {
+        } else if (arguments[0] == "divide") {
             System.out.println(calc.divide(arg1, arg2));
-        } else if (args[0] == "fibonacci") {
+        } else if (arguments[0] == "fibonacci") {
             System.out.println(calc.fibonacciNumberFinder(arg1));
-        } else if (args[0] == "binary") {
+        } else if (arguments[0] == "binary") {
             System.out.println(calc.intToBinaryNumber(arg1));
         }
     }
